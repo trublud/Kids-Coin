988e87091940561b02bb056ab4ee2dff6e3525d7
trublud <xmindpingx@gmail.com>
2019-07-08 21:02:52 -0700

    Update chainparamsbase.cpp

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -33,7 +33,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9332;
+        nRPCPort = 1976;
     }
 };
 
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19332;
+        nRPCPort = 1977;
         strDataDir = "testnet4";
     }
 };
